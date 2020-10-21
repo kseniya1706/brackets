ab75f6d0eba051994ec63df41bd64e9332dd3fd7
Kseniya <55946556+kseniya1706@users.noreply.github.com>
2020-10-22 01:19:14 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  const arrFromStr = str.split('');
+
+  const length = arrFromStr.length;
+  let count = 0;
+
+  for (let i = 0; i <= length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (arrFromStr[i] === bracketsConfig[j][0] &&
+          arrFromStr[i + 1] == bracketsConfig[j][1]) {
+            arrFromStr.splice(i, 2);
+                i = -1;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    return arrFromStr.length === 0 ? true : false
 }
